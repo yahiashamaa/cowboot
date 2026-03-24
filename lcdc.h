cdbc2f17d9740de811d37c0ef3a9a942ea011991
yahiashamaa <yehiashamaa987@gmail.com>
2026-03-24 13:12:20 +0200

    lcdc.c: Split all display init related functions to a seperate file

---
+++ lcdc.h
@@ -0,0 +1,19 @@
+#ifndef LCDC_H_
+#define LCDC_H_
+
+#include "types.h"
+
+struct framedesc {
+    u32 next;
+    u32 databuf;
+    u32 id;
+    u32 cmd;
+    u32 offsize;
+    u32 page_width;
+    u32 cpos;
+    u32 desc_size;
+} framedesc;
+
+void early_lcdc_init(void);
+
+#endif // LCDC_H_
