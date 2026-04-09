4724312ddc717684bf71c90266cfc928a7429723
yahiashamaa <yehiashamaa987@gmail.com>
2026-04-09 02:15:35 +0200

    console: Add the simplest implementation of a 'console'
    it's really just a wrapper for draw_string hehehe
    
    Signed-off-by: Yahia Shamaa <yehiashamaa987@gmail.com>

---
+++ src/console.c
@@ -0,0 +1,13 @@
+#include "inc/types.h"
+#include "inc/io.h"
+#include "inc/fb.h"
+
+// TODO: add support for format specifiers :<
+
+int video_printf(const char *fmt, ...)
+{
+    draw_string(cur_x, cur_y, fmt, 0xFFFFFFFF, 0x00000000);
+    cur_x = 50;
+    cur_y += 16;
+    return 0;
+}
---
+++ src/inc/console.h
@@ -0,0 +1,6 @@
+#ifndef CONSOLE_H_
+#define CONSOLE_H_
+
+int video_printf(const char *fmt, ...);
+
+#endif // CONSOLE_H_
